d1f3c304640efbfded582c22794715b0f43dade8
Andrey <killerus34@gmail.com>
2019-03-30 14:20:38 +0300

    1.2

--- Ccc1/main.c
+++ Ccc1/main.c
@@ -7,8 +7,8 @@ int C, i, size, Arr[20];
 printf("\n Array containing 20 Random Numbers\n");
 for(i=0;i<20;i++)
 {
-Arr[i]=rand();
-printf("%d\n",Arr[i]);
+    Arr[i]=rand();
+    printf("%d\n",Arr[i]);
 }
 C=Arr[0];
 for(i=0;i<20;i++)
