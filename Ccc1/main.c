94aa478a12522b29af7e8ebe2cbfd42c98b4fd7b
Andrey <killerus34@gmail.com>
2019-03-30 14:27:53 +0300

    1.3

--- Ccc1/main.c
+++ Ccc1/main.c
@@ -3,12 +3,21 @@
 #include <math.h>
 int main()
 {
-int C, i, size, Arr[20];
+int C=0, i, size, Arr[20];
 printf("\n Array containing 20 Random Numbers\n");
 for(i=0;i<20;i++)
 {
     Arr[i]=rand();
     printf("%d\n",Arr[i]);
+    if(C>=Arr[i])
+    {
+        printf("C=%d\n",C);
+    }
+    else
+    {
+        C=Arr[i];
+        printf("C=%d\n",C);
+    }
 }
 C=Arr[0];
 for(i=0;i<20;i++)
